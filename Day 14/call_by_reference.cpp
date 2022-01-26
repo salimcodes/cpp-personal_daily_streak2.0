d291a8a02dabdcbfdb34b1dc3091440732aa14c7
Salim Ọlánrewájú Oyinlọlá <salimoyinlola@gmail.com>
2022-01-26 04:12:58 +0100

    Add files via upload

---
+++ Day 14/call_by_reference.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+//Random Example to revise Call by reference
+
+
+void GetTwoNumbers(float*, float*);
+
+
+int main()
+{
+	cout << "My name is Salim." << endl;
+	float Radius, Height;
+	GetTwoNumbers(&Radius, &Height);
+
+	float volume;
+	volume = Radius * Height;
+	cout << "The volume is: " << volume << endl;
+	system("Pause");
+}
+
+void GetTwoNumbers(float *a_ptr, float *b_ptr)
+{
+	float letter_a, letter_b;
+	cout << "Enter the first number: " << endl;
+	cin >> letter_a;
+	cout << "Enter the second number: " << endl;
+	cin >> letter_b;
+
+	*a_ptr = letter_a;
+	*b_ptr = letter_b;
+}
