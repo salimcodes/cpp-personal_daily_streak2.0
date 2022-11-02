664e2e2c4c0c95ae7c0179db54a8276e87b25c24
Salim Oyinlola <salimoyinlola@gmail.com>
2022-11-02 04:03:54 +0100

    Add files via upload

---
+++ Day 56/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "sum.h"
+
+int main()
+{
+	int s = 4, f = 3;
+	cout << "The sum of the numbers is " << sum(s, f) << endl;
+
+	system("Pause");
+	return 0;
+}
